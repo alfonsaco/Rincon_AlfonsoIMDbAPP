078ba2c6667c625d3c95abccd34ce6ef63d1b48e
alfonsaco <alfonso.rincondev@gmail.com>
2025-01-08 16:30:34 +0100

    Cambio del texto del botón de Google

--- app/src/main/java/edu/pruebas/rincon_alfonsoimdbapp/LoginActivity.java
+++ app/src/main/java/edu/pruebas/rincon_alfonsoimdbapp/LoginActivity.java
@@ -1,12 +1,15 @@
 package edu.pruebas.rincon_alfonsoimdbapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.common.SignInButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,6 +18,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        // Obtenemos el botón, y cambiamos su texto
+        SignInButton signInButton = findViewById(R.id.btnSignIn);
+        ((TextView) signInButton.getChildAt(0)).setText("Sign in with Google");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -22,3 +30,5 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 }
+
+
